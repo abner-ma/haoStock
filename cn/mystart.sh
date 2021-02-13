0cc235749ece4b4501b6f5eccdc9040849cec5f8
abner-ma <969023674@qq.com>
2021-02-13 19:50:56 +0800

    Create mystart.sh

---
+++ cn/mystart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+WORK_DIR=`pwd`
+PYTHON_CMD=python3
+VERIFY=$WORK_DIR/common/verifydate.py
+MY_PROC=$WORK_DIR/main/main.py
+
+cd $WORK_DIR
+$PYTHON_CMD $VERIFY
+if [ $? = 1 ]
+then
+        $PYTHON_CMD $MY_PROC -o result.txt
+fi
